e3c832e8630e6a3251c7f7547dbf8b466ffb0d34
jyb <472950043@qq.com>
2016-01-11 16:43:09 +0800

    第一版

---
+++ NiceRadar/NiceRadar/RadarView/CircleView.swift
@@ -0,0 +1,21 @@
+//
+//  CircleView.swift
+//  NiceRadar
+//
+//  Created by jyb on 16/1/11.
+//  Copyright © 2016年 jyb. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ NiceRadar/NiceRadar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NiceRadar
+//
+//  Created by jyb on 16/1/11.
+//  Copyright © 2016年 jyb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
